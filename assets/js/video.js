aa16c15b4db3de335488e6f427fd8a64f65e919c
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2023-06-06 18:37:44 -0300

    add videos cim

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,23 +1,40 @@
-var videoIds = ['d3VGlOen-o0', '0IVl1B58zVE', 'sBA_SDhIPqQ', '5yJ_cAUrpQc'];
+// var videoIds = ['d3VGlOen-o0', '0IVl1B58zVE', 'sBA_SDhIPqQ', '5yJ_cAUrpQc'];
 
-// Variável global para armazenar os players do YouTube
-var players = [];
+// // Variável global para armazenar os players do YouTube
+// var players = [];
 
-// Função de inicialização dos players
-function onYouTubeIframeAPIReady() {
-    // Criação dos players
-    for (var i = 0; i < videoIds.length; i++) {
-        var player = new YT.Player('player' + (i + 1), {
-            videoId: videoIds[i],
-            events: {
-                'onReady': onPlayerReady
-            }
-        });
-        players.push(player);
-    }
-}
+// // Função de inicialização dos players
+// function onYouTubeIframeAPIReady() {
+//     // Criação dos players
+//     for (var i = 0; i < videoIds.length; i++) {
+//         var player = new YT.Player('player' + (i + 1), {
+//             videoId: videoIds[i],
+//             events: {
+//                 'onReady': onPlayerReady
+//             }
+//         });
+//         players.push(player);
+//     }
+// }
 
-// Função executada quando um player estiver pronto
-function onPlayerReady(event) {
+// // Função executada quando um player estiver pronto
+// function onPlayerReady(event) {
     
-}
+// }
+
+
+//main banner 
+
+// Aguarde o carregamento completo da página
+window.addEventListener('load', function() {
+    var video = document.getElementById('bg-video');
+    var botaoPlay = document.getElementById('botao-play');
+
+    if (video && botaoPlay) {
+      botaoPlay.addEventListener('click', function() {
+        video.play();
+      });
+
+      botaoPlay.click();
+    }
+  });
