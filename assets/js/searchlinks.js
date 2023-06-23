fce9c636579257ded8736c3fdf30590325d64e7e
gustavolopesnobrega <gustavo.lopes.nobrega@gmail.com>
2023-06-22 22:26:38 -0300

    set await function

--- assets/js/searchlinks.js
+++ assets/js/searchlinks.js
@@ -1,5 +1,4 @@
-
-function search() {
+async function search() {
   var input = document.getElementById('searchInput');
   var filter = input.value.toUpperCase();
   var searchResults = document.getElementById('searchResults');
@@ -27,7 +26,6 @@ function search() {
     {
       url: 'delegazioni.html',
       pagename: 'Delegazioni'
-
     },
     {
       url:'fotos.html',
@@ -45,45 +43,56 @@ function search() {
       url: 'presentazione.html',
       pagename: 'Presentazione'
     }
-    
   ];
 
-  
- 
+  // Array para armazenar os resultados encontrados
+  var foundResults = [];
+
+  // Função para fazer uma requisição assíncrona
+  function makeRequest(url) {
+    return new Promise(function(resolve, reject) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var pageContent = this.responseText.toUpperCase();
+
+          // Verificar se o termo de pesquisa está presente no conteúdo da página
+          if (pageContent.includes(filter)) {
+            var resultItem = {
+              url: url.url,
+              pagename: url.pagename
+            };
+            foundResults.push(resultItem);
+          }
+          resolve();
+        }
+      };
+      xhttp.open('GET', url.url, true);
+      xhttp.send();
+    });
+  }
+
   // Loop pelas URLs das páginas
-  pageURLs.forEach(function(url) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var pageContent = this.responseText.toUpperCase();
+  for (var i = 0; i < pageURLs.length; i++) {
+    await makeRequest(pageURLs[i]);
+  }
 
-        // Verificar se o termo de pesquisa está presente no conteúdo da página
-        if (pageContent.includes(filter)) {
-          var resultItem = document.createElement('li');
-          var link = document.createElement('a');
-          link.href = url.url;
-          link.textContent = url.pagename // Remover a extensão .html
-          resultItem.appendChild(link);
-          searchResults.appendChild(resultItem);
-          foundResults = true; // Marcar que pelo menos um resultado foi encontrado
-          notfound.remove(resultItem)
-        } 
-      }
-    };
-    xhttp.open('GET', url.url, true);
-    xhttp.send();
-  });
-
-  // Variável para controlar se algum resultado foi encontrado
-  var foundResults = false;
-
-  if (foundResults == false) {
+  if (foundResults.length > 0) {
+    // Resultados encontrados
+    foundResults.forEach(function(result) {
+      var resultItem = document.createElement('li');
+      var link = document.createElement('a');
+      link.href = result.url;
+      link.textContent = result.pagename;
+      resultItem.appendChild(link);
+      searchResults.appendChild(resultItem);
+    });
+  } else {
+    // Nenhum resultado encontrado
     var resultItem = document.createElement('li');
     var notfound = document.createElement('h6');
-    notfound.textContent = "Spiacente, nessun file trovato." 
+    notfound.textContent = 'Spiacente, nessun file trovato.';
     resultItem.appendChild(notfound);
     searchResults.appendChild(resultItem);
   }
 }
-
-  
